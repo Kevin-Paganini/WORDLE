bb76dda137031ac5d0964496c329046be816e52a
meredithc01 <meredithc@msoe.edu>
2022-03-15 21:10:59 -0500

    Made the keyboard show when letters have been used

--- src/wordle/Controller.java
+++ src/wordle/Controller.java
@@ -13,7 +13,8 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 
-import javax.xml.soap.Text;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 
 public class Controller {
@@ -25,6 +26,7 @@ public class Controller {
             "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M");
     int position;
     GridPane grid_input;
+    GridPane letters_used;
 
     /**
      * Main Grid of the application
@@ -48,14 +50,13 @@ public class Controller {
         grid_input = createGridOfInputs(6, 5);
 
         // Create keyboard of used letters
-        GridPane letters_used = createKeyBoardInputs();
+        letters_used = createKeyBoardInputs();
         letters_used.setLayoutX(400);
         letters_used.setLayoutY(50);
         letters_used.getStyleClass().add("keyBoardGrid");
 
         // Adding all to main pane
         MAIN_PANE.getChildren().addAll(grid_input, letters_used);
-
     }
 
     /**
@@ -138,6 +139,11 @@ public class Controller {
             tf.setDisable(true);
             input += tf.getText();
         }
+        char letter;
+        for(int i = 0; i < input.length(); ++i) {
+            letter = input.charAt(i);
+            checkLetter(letter);
+        }
         System.out.println(input);
         guess++;
         for(int i = 0; i < numLetters; i++){
@@ -186,5 +192,114 @@ public class Controller {
         }
     }
 
+    private void checkLetter(char letter) {
+        Label box;
+        switch (letter){
+            case 'Q':
+                box = (Label) letters_used.getChildren().get(0);
+                box.setDisable(true);
+                break;
+            case 'W':
+                box = (Label) letters_used.getChildren().get(1);
+                box.setDisable(true);
+                break;
+            case 'E':
+                box = (Label) letters_used.getChildren().get(2);
+                box.setDisable(true);
+                break;
+            case 'R':
+                box = (Label) letters_used.getChildren().get(3);
+                box.setDisable(true);
+                break;
+            case 'T':
+                box = (Label) letters_used.getChildren().get(4);
+                box.setDisable(true);
+                break;
+            case 'Y':
+                box = (Label) letters_used.getChildren().get(5);
+                box.setDisable(true);
+                break;
+            case 'U':
+                box = (Label) letters_used.getChildren().get(6);
+                box.setDisable(true);
+                break;
+            case 'I':
+                box = (Label) letters_used.getChildren().get(7);
+                box.setDisable(true);
+                break;
+            case 'O':
+                box = (Label) letters_used.getChildren().get(8);
+                box.setDisable(true);
+                break;
+            case 'P':
+                box = (Label) letters_used.getChildren().get(9);
+                box.setDisable(true);
+                break;
+            case 'A':
+                box = (Label) letters_used.getChildren().get(10);
+                box.setDisable(true);
+                break;
+            case 'S':
+                box = (Label) letters_used.getChildren().get(11);
+                box.setDisable(true);
+                break;
+            case 'D':
+                box = (Label) letters_used.getChildren().get(12);
+                box.setDisable(true);
+                break;
+            case 'F':
+                box = (Label) letters_used.getChildren().get(13);
+                box.setDisable(true);
+                break;
+            case 'G':
+                box = (Label) letters_used.getChildren().get(14);
+                box.setDisable(true);
+                break;
+            case 'H':
+                box = (Label) letters_used.getChildren().get(15);
+                box.setDisable(true);
+                break;
+            case 'J':
+                box = (Label) letters_used.getChildren().get(16);
+                box.setDisable(true);
+                break;
+            case 'K':
+                box = (Label) letters_used.getChildren().get(17);
+                box.setDisable(true);
+                break;
+            case 'L':
+                box = (Label) letters_used.getChildren().get(18);
+                box.setDisable(true);
+                break;
+            case 'Z':
+                box = (Label) letters_used.getChildren().get(19);
+                box.setDisable(true);
+                break;
+            case 'X':
+                box = (Label) letters_used.getChildren().get(20);
+                box.setDisable(true);
+                break;
+            case 'C':
+                box = (Label) letters_used.getChildren().get(21);
+                box.setDisable(true);
+                break;
+            case 'V':
+                box = (Label) letters_used.getChildren().get(22);
+                box.setDisable(true);
+                break;
+            case 'B':
+                box = (Label) letters_used.getChildren().get(23);
+                box.setDisable(true);
+                break;
+            case 'N':
+                box = (Label) letters_used.getChildren().get(24);
+                box.setDisable(true);
+                break;
+            case 'M':
+                box = (Label) letters_used.getChildren().get(25);
+                box.setDisable(true);
+                break;
+        }
+    }
 
 }
