e3318e9b52de3aca151f62dcb26d157e957a6ac5
paganinik <paganinik@msoe.edu>
2022-03-18 14:06:13 -0500

    added style class for win alert

--- src/wordle/Controller.java
+++ src/wordle/Controller.java
@@ -4,10 +4,7 @@ import javafx.beans.Observable;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
@@ -277,7 +274,11 @@ public class Controller {
 
     private void showWinAlert() {
         Alert a = new Alert(Alert.AlertType.CONFIRMATION);
+        DialogPane d = a.getDialogPane();
+        d.getStyleClass().clear();
+        d.getStyleClass().add("winner-dialog");
         a.showAndWait();
+
         if(true){
             // If player wants to play again and clicks ok
             // Record stats
