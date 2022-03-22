b139cf10db5fffb5803c1396292e9ce8dcd9eef7
paganinik <paganinik@msoe.edu>
2022-03-22 07:56:34 -0500

    statistics possible classes

--- src/Statistics/Session.java
+++ src/Statistics/Session.java
@@ -1,4 +1,28 @@
 package Statistics;
 
+import wordle.Wordle;
+
+import java.util.ArrayList;
+
 public class Session {
+    private ArrayList<Wordle> sessionGames;
+
+    public Session (){
+        sessionGames  = new ArrayList<>();
+
+    }
+
+    public void addGame(Wordle wordle){
+        sessionGames.add(wordle);
+    }
+
+    public int averageGuesses(){
+        int total = 0;
+        //sessionGames.stream().reduce(x -> );
+        return 0;
+    }
+
+    public int getAvgGuesses(){
+        return 0;
+    }
 }
