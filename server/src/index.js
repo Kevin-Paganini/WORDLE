91a0eda71e8bf68aff6666eac607af5981b7fece
paganinik <paganinik@msoe.edu>
2022-04-23 17:16:42 -0500

    package.json

---
+++ server/src/index.js
@@ -0,0 +1,43 @@
+const http = require("http");
+const fs = require("fs").promises;
+
+
+
+
+const host = 'localhost';
+const port = 8000;
+
+
+const requestHandler = function(req, res) {
+    if (req.method === 'GET') {
+        fs.readFile('index.html') // read html file
+        .then(contents => {
+            res.setHeader('Content-Type', 'text/html');
+            res.writeHead(200); //status code
+            res.end(contents); // add files to response
+        })
+        .catch(err => {
+            res.writeHead(500); // error
+            res.end(err); 
+            return;
+
+        });
+    } else if (req.method === 'POST'){
+        var body = "";
+        req.on("data", function(chunk){
+            body += chunk;
+            console.log(body);
+        });
+    }
+    
+};
+
+
+
+
+const server = http.createServer(requestHandler);
+server.listen(port, host, () => {
+    console.log('Server is running on http://localhost:8000');
+
+  
+});
