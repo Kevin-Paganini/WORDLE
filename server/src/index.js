062b898ce8901edcc924b845e5d9bd6c347ed3cc
paganinik <paganinik@msoe.edu>
2022-04-30 15:31:07 -0500

    ?

--- server/src/index.js
+++ server/src/index.js
@@ -25,7 +25,7 @@ http.createServer(function (req, res) {
         req.on('data', chunk => chunks.push(chunk));
         req.on('end', () => {
         const byte_data = Buffer.concat(chunks);
-        const string_data = qs.parse(byte_data.toString());
+        const string_data = byte_data.toString();
         
         console.log('Data: ', string_data);
         fs.writeFile(appRoot + '/chart_gen/GlobalData.txt', "hey there", 
