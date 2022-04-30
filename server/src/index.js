912aabc187ab7c63cc80a143726e2cc8533d6d3b
paganinik <paganinik@msoe.edu>
2022-04-30 14:41:00 -0500

    hi

--- server/src/index.js
+++ server/src/index.js
@@ -27,12 +27,7 @@ http.createServer(function (req, res) {
         const byte_data = Buffer.concat(chunks);
         const string_data = qs.parse(byte_data.toString());
         console.log('Data: ', string_data);
-        fs.appendFile('chart_gen/GlobalData.txt', string_data, err => {
-            if (err) {
-                console.error(err);
-            }
-            // done!
-        });
+        
         });
         return res.end();
     }
