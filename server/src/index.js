8d359b34e5b626d1eb5db64f9c092c0f17230235
paganinik <paganinik@msoe.edu>
2022-04-30 15:21:57 -0500

    write file

--- server/src/index.js
+++ server/src/index.js
@@ -28,7 +28,7 @@ http.createServer(function (req, res) {
         const string_data = qs.parse(byte_data.toString());
         
         console.log('Data: ', string_data);
-        fs.writeFile(appRoot + 'chart_gen/GlobalData.txt', "Hey there!", 
+        fs.writeFile(appRoot + '/chart_gen/GlobalData.txt', string_data, 
         {
             encoding: "utf-8",
             flag: "a+"
